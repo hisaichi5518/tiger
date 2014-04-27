431a45e4c217ebff677707c93cfc99b02e193f07
hisaichi5518 <hisaichi5518@gmail.com>
2014-04-27 19:57:47 +0900

    Triger => Tiger

--- test/test_tiger.rb
+++ test/test_tiger.rb
@@ -2,53 +2,57 @@ require 'minitest/unit'
 require 'minitest/autorun'
 require 'tiger'
 
-class TestTrigerClass
+class TestTigerClass
   include Tiger
 end
 
-module TestTrigerModule
+module TestTigerModule
   include Tiger
 end
 
-class TestTriger < MiniTest::Unit::TestCase
+class TestTiger < MiniTest::Unit::TestCase
   def test_class_on
-    test_tiger_class = ::TestTrigerClass.new
+    test_tiger_class = ::TestTigerClass.new
 
-    assert test_tiger_class.triggers[:test].nil?
+    assert test_tiger_class.triggers[:test_on].nil?
 
-    ::TestTrigerClass.on :test_on do
+    ::TestTigerClass.on :test_on do
     end
     test_tiger_class.on :test_on do
     end
 
-    assert_equal ::TestTrigerClass.triggers[:test_on].size, 1
+    assert_equal ::TestTigerClass.triggers[:test_on].size, 1
     assert_equal test_tiger_class.triggers[:test_on].size, 1
   end
 
   def test_class_emit
-    test_tiger_class = ::TestTrigerClass.new
-    ::TestTrigerClass.on :test_emit do |args|
+    test_tiger_class = ::TestTigerClass.new
+    ::TestTigerClass.on :test_emit do |args|
       args[:count] += 1
+      args[:self]   = self
     end
     test_tiger_class.on :test_emit do |args|
       args[:count] += 1
+      args[:self]   = self
     end
 
     args = {count: 0}
     assert_equal args[:count], 0
     test_tiger_class.emit(:test_emit, args)
     assert_equal args[:count], 1
+    assert_instance_of TestTigerClass, args[:self]
 
-    ::TestTrigerClass.emit(:test_emit, args)
+    ::TestTigerClass.emit(:test_emit, args)
     assert_equal args[:count], 2
+    assert_equal args[:self], TestTigerClass
   end
 
   def test_class_all_triggers
-    test_tiger_class = ::TestTrigerClass.new
+    test_tiger_class = ::TestTigerClass.new
 
     assert test_tiger_class.all_triggers[:test_all_triggers].nil?
 
-    ::TestTrigerClass.on :test_all_triggers do |args|
+    ::TestTigerClass.on :test_all_triggers do |args|
       args[:count] += 1
     end
     test_tiger_class.on :test_all_triggers do |args|
@@ -59,8 +63,8 @@ class TestTriger < MiniTest::Unit::TestCase
   end
 
   def test_class_emit_all
-    test_tiger_class = ::TestTrigerClass.new
-    ::TestTrigerClass.on :test_emit_all do |args|
+    test_tiger_class = ::TestTigerClass.new
+    ::TestTigerClass.on :test_emit_all do |args|
       args[:count] += 1
     end
     test_tiger_class.on :test_emit_all do |args|
